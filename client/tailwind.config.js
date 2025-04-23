3d2a57778f1cd1dbbf958deb8b49dd02cbe8de60
Yony Marian <yonyon.marian@gmail.com>
2025-04-22 21:40:42 -0700

    re-init with better tech stack

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+      "./src/**/*.css", // Add this line to include CSS files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
