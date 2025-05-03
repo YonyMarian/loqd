1dc28e80defb67ffe3b9565537c8ac9b8cf66d2e
yu-kate <yu.katelyna@gmail.com>
2025-05-02 20:13:34 -0700

    parser draft - no work

--- client/src/components/Upload.tsx
+++ client/src/components/Upload.tsx
@@ -3,11 +3,17 @@ import React from 'react';
 const Upload: React.FC = () => {
     return (
         <>
-            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            {/* <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
                 <h1 className="text-4xl font-bold mb-4">Upload Your File</h1>
                 <input type="file" className="mb-4" />
                 <button className="bg-blue-500 text-white px-4 py-2 rounded">Upload a calendar file</button>
-            </div>
+            </div> */}
+
+            <form id="uploadForm" encType="multipart/form-data">
+                <input type="file" name="calendarFile" />
+                <button type="submit">Upload</button>
+            </form>
+            <script src="./upload.js"></script>
         </>
     )
 }
