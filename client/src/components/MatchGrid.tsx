79b7020c66ecdb8433f51140cbb28df70b680913
Lian Elsa Linton <lianelsalinton@gmail.com>
2025-05-13 14:13:53 -0700

    created dashboard layout

---
+++ client/src/components/MatchGrid.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import MatchProfile from './MatchProfile';
+import '../styles/MatchProfile.css';
+
+const MatchGrid: React.FC = () => {
+  return (
+    <div className="match-grid">
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+
+            <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+      <MatchProfile />
+    </div>
+  );
+};
+
+export default MatchGrid;
---
+++ client/src/components/MatchProfile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/MatchProfile.css';
+
+const MatchProfile: React.FC = () => {
+  return (
+    <div className="match-profile-box">
+        <h3>Match Profile</h3>
+        <p>This is your main profile box. Add fields or components as needed.</p>
+    </div>
+  );
+};
+
+export default MatchProfile;
