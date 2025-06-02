5030a3e4b6d9853bc4d5cc00869637b4f62253f8
Yony Marian <yonyon.marian@gmail.com>
2025-06-01 20:59:33 -0700

    fix signin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/account" element={<Account />} />
+        <Route path="/dashboard" element={<Account />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
